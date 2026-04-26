947164b194cab7dd5b662fa729993f847b27cdf0
alfiebbik <sonicspeedyboy101@gmail.com>
2026-04-26 04:47:31 +0100

    Rename Initialize function to Init and simplify DllMain

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -1,17 +1,7 @@
 #include <windows.h>
 
-extern "C" __declspec(dllexport) void Initialize() {
-    // This is a placeholder for your BikBot logic
-}
+extern "C" __declspec(dllexport) void Init() {}
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
-    switch (ul_reason_for_call) {
-        case DLL_PROCESS_ATTACH:
-            break;
-        case DLL_THREAD_ATTACH:
-        case DLL_THREAD_DETACH:
-        case DLL_PROCESS_DETACH:
-            break;
-    }
     return TRUE;
 }
