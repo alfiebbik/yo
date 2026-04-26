e4ef8b32ba260158ab64b268c5f2893fb0d76937
alfiebbik <sonicspeedyboy101@gmail.com>
2026-04-26 04:38:30 +0100

    Update dllmain.cpp

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -1,10 +1,15 @@
-#include <amethyst/runtime/AmethystRuntime.h>
-#include <amethyst/ui/NinesliceHelper.h>
+#include <Windows.h>
 
-void OnStartJoinServer(Amethyst::EventContext& ctx) {
-    ctx.player.displayClientMessage("§bBikBotClient §7successfully loaded!");
+// This is the most basic entry point that won't throw errors
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved) {
+    switch (ul_reason_for_call) {
+    case DLL_PROCESS_ATTACH:
+        // This will show a message when the client loads on your Xbox/PC
+        MessageBoxA(NULL, "BikBotClient Loaded!", "Success", MB_OK);
+        break;
+    }
+    return TRUE;
 }
 
-extern "C" __declspec(dllexport) void Initialize(Amethyst::Context& ctx) {
-    ctx.events.onStartJoinServer.add(&OnStartJoinServer);
-}
+// Exporting a dummy function to ensure the DLL actually builds
+extern "C" __declspec(dllexport) void Init() {}
