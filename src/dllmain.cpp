bfd7a86441ca6ea7aeb14f988e8d44bfca94a996
alfiebbik <sonicspeedyboy101@gmail.com>
2026-04-26 04:45:05 +0100

    Refactor DllMain and add Initialize function
    Removed message box on DLL load and added Initialize function.

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -1,15 +1,17 @@
-#include <Windows.h>
+#include <windows.h>
 
-// This is the most basic entry point that won't throw errors
-BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved) {
+extern "C" __declspec(dllexport) void Initialize() {
+    // This is a placeholder for your BikBot logic
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
     switch (ul_reason_for_call) {
-    case DLL_PROCESS_ATTACH:
-        // This will show a message when the client loads on your Xbox/PC
-        MessageBoxA(NULL, "BikBotClient Loaded!", "Success", MB_OK);
-        break;
+        case DLL_PROCESS_ATTACH:
+            break;
+        case DLL_THREAD_ATTACH:
+        case DLL_THREAD_DETACH:
+        case DLL_PROCESS_DETACH:
+            break;
     }
     return TRUE;
 }
-
-// Exporting a dummy function to ensure the DLL actually builds
-extern "C" __declspec(dllexport) void Init() {}
