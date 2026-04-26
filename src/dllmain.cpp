c7e694992280cca7cde6136fc7b5ca08b94a56f9
alfiebbik <sonicspeedyboy101@gmail.com>
2026-04-26 04:34:19 +0100

    Refactor UI handling and add server join event
    Removed UI rendering code and added server join message.

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -1,41 +1,10 @@
-﻿#include <amethyst/ui/ImGui.hpp>
+#include <amethyst/runtime/AmethystRuntime.h>
+#include <amethyst/ui/NinesliceHelper.h>
 
-bool showMenu = false;
-
-// This runs every time a frame is drawn on your screen
-void OnRenderUI() {
-    // Listen for the 'P' key (Virtual Key 0x50)
-    if (ImGui::IsKeyPressed(ImGuiKey_P)) {
-        showMenu = !showMenu; 
-    }
-
-    if (showMenu) {
-        // Start the Meteor-style Window
-        ImGui::Begin("Meteor Bedrock Edition", &showMenu);
-        ImGui::Text("Version 1.0 - Built by You");
-        ImGui::Separator();
-
-        // Checkboxes for your hacks
-        ImGui::Checkbox("Flight", &flyEnabled);
-        ImGui::Checkbox("Speed", &speedEnabled);
-        ImGui::Checkbox("Killaura", &killauraEnabled);
-        ImGui::Checkbox("Auto-Walk", &autoWalkEnabled);
-        ImGui::Checkbox("Auto-Mine", &autoMineEnabled);
-        ImGui::Checkbox("Stash Finder", &stashFinderEnabled);
-        
-        if (ImGui::Button("Disable All")) {
-            flyEnabled = speedEnabled = killauraEnabled = false;
-        }
-
-        ImGui::End();
-    }
+void OnStartJoinServer(Amethyst::EventContext& ctx) {
+    ctx.player.displayClientMessage("§bBikBotClient §7successfully loaded!");
 }
 
-// Update your Initialize function to include the UI
-void Initialize(AmethystContext* ctx) {
-    ctx->events.beforeTick.AddListener(&OnTick);
-    ctx->events.onFrameUpdate.AddListener(&OnUpdate);
-    
-    // This line tells Amethyst to start drawing your menu
-    ctx->events.onRenderUI.AddListener(&OnRenderUI);
+extern "C" __declspec(dllexport) void Initialize(Amethyst::Context& ctx) {
+    ctx.events.onStartJoinServer.add(&OnStartJoinServer);
 }
